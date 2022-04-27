0594f3943b29069fa025912aad4d5f76da944c49
Serje <Serje.havandjian@gmail.com>
2022-04-27 11:31:27 -0400

    commit tree a7f0a38d60f5a8cb342d7bdb7d78e80cf49f84fc

--- index.js
+++ index.js
@@ -10,7 +10,66 @@ const tutorials = [
   'what is the difference between event capturing and bubbling?',
   'what is JSONP?'
 ];
+console.log(tutorials.length)
 
-const titleCased = () => {
-  return tutorials
+
+function titleCased(){
+  return tutorials.map((tutorial)=>{
+    // split apart each tutorial into individual words
+    const tutorialWords = tutorial.split(" ")
+    // capitalize each individual word
+    const tutorialWordsUpper = tutorialWords.map((tutorialWord)=>{
+      return tutorialWord[0].toUpperCase() + tutorialWord.substring(1)
+    })
+    // put back each word into a sentence in tutorial
+    return tutorialWordsUpper.join(" ")
+  })
 }
+
+
+
+
+
+
+// const titleCased = tutorials.map(createTitleCase)
+
+// function createTitleCase(tutorials){
+//   // let newTutorials = tutorials.split(" ")
+//   // console.log(newTutorials)
+//   // newTutorials = newTutorials[0][0].toUpperCase()
+//   // return newTutorials
+//   let newArray = []
+//   for (let i = 0; i < tutorials.length; i++){
+//     let split = tutorials[i].split()
+    
+//     newArray.push(split[0])
+//     console.log(newArray)
+//   }
+//   }
+
+
+
+// const titleCased = tutorials.map((create) => {
+//   newTutorials = tutorial.toLocaleLowerCase()
+  
+//   for(let i = 0; i < newTutorials.length; i++){
+//     newTutorials.split("")
+//     return newTutorials
+//   }
+
+//   return newTutorials
+//   newTutorials.split("")
+//   // parse each word in each sentence in each element
+//   // capitalize first letter of each word
+//   // put back sentence
+//   // move to next sentence
+
+// })
+
+
+
+
+
+// const titleCased = () => {
+//   return tutorials
+// }
